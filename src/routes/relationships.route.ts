d6ba4caa8929bb66ab1a9bfedf6e8b32df304e52
BinyaminYacovson <bj57577@gmail.com>
2024-12-18 13:04:35 +0200

    Add initial controllers and routes for analysis and relationships

---
+++ src/routes/relationships.route.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+const router = Router();
+
+router.get('/top-groups', );
+router.get('/groups-by-year', );
+router.get('/deadliest-regions', );
+
+export default router;
