1343b153ecd4b8aa1427e4aeaff9ee06d5772f9c
BinyaminYacovson <bj57577@gmail.com>
2024-12-18 13:03:52 +0200

    Refactor terrorEventModel to enforce required fields and update model creation logic for dynamic collection names

--- src/models/terrorEventModel.ts
+++ src/models/terrorEventModel.ts
@@ -2,23 +2,26 @@ import { model, Schema } from "mongoose";
 
 export const terrorAttackSchema = new Schema({
   eventid: { type: Number, unique: true },
-  iyear: { type: Number },
-  imonth: { type: Number },
-  iday: { type: Number },
-  country_txt: { type: String },
-  region_txt: { type: String },
+  iyear: { type: Number, required: true },
+  imonth: { type: Number, required: true },
+  iday: { type: Number, required: true },
+  country_txt: { type: String, required: true },
+  region_txt: { type: String, required: true },
   city: { type: String },
   latitude: { type: Number },
   longitude: { type: Number },
   attacktype1_txt: { type: String },
   targtype1_txt: { type: String },
   target1: { type: String },
-  gname: { type: String },
+  gname: { type: String, required: true },
   weaptype1_txt: { type: String },
   nkill: { type: Number },
   nwound: { type: Number },
-  nperps: { type: Number }, /* number of participants in the attack */
+  nperps: { type: Number },
   summary: { type: String }
 });
 
-export const TerrorEventModel = model("Event", terrorAttackSchema);
+export const terrorEventModel = (year: number, region: string) => {
+  const collectionName = `Event_${year}_${region.replace(/\s+/g, '_')}`;
+  return model(collectionName, terrorAttackSchema);
+};
