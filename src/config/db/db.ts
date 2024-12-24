7b4d96ab5a79c62ce3117fa73d2523e0ab86006c
BinyaminYacovson <bj57577@gmail.com>
2024-12-24 10:48:09 +0200

    Update MongoDB connection string to use Atlas cluster

--- src/config/db/db.ts
+++ src/config/db/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectToMongo = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/Terror-attacks');
+    await mongoose.connect('mongodb+srv://binyamin:beny@cluster0.ssafa.mongodb.net/terror-attacks');
     console.log('[database] MongoDB successfully connected');
   } catch (err) {
     console.error('[database] Connection error:', err);
